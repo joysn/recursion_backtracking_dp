55810c8253f1a0d6164c96113e364924310caa95
joysn <joysn1980@yahoo.com>
2022-07-04 08:30:20 +0530

    Add files via upload

--- recursion/friendsParty.py
+++ recursion/friendsParty.py
@@ -10,8 +10,24 @@ def partyCount(n):
     return (n-1)*partyCount(n-2) + partyCount(n-1)
 
 
+def partyCountTab():
+    
+    dp = [0 for i in range(n+1)]
+    dp[0] = 1
+    if n >= 1:
+        dp[1] = 1
+        for i in range(2,n+1):
+            dp[i] = (i-1)*dp[i-2] + dp[i-1]
+
+    return dp[-1]
+
 if __name__=="__main__":
     
+    print("Top Down",)
     for n in range(11):
         print(partyCount(n), end = " ")
+    print()
+    print("Bottom Up")
+    for n in range(11):
+        print(partyCountTab(), end = " ")
     
